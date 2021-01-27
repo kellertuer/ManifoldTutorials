6ad9c51f11055782d4e2927737cb8cb7340af2e9
Ronny <git@ronnybergmann.net>
2021-01-27 20:48:49 +0100

    Update Bootstrap, add Fontawesome, sketch preferences.

--- _code/rayleigh.jl
+++ _code/rayleigh.jl
@@ -9,8 +9,8 @@ M = Sphere(n - 1)
 
 # Define the problem cost function and its Riemannian gradient.
 F(X) = -X' * A * X
-∇F(X) = -2 * (A * X - X * X' * A * X)
+gradF(X) = -2 * (A * X - X * X' * A * X)
 
 # Solve
 x = random_point(M)
-quasi_Newton(M, F, ∇F, x; debug=[:Iteration, " ", :Cost, "\n", 1, :Stop])
+quasi_Newton(M, F, gradF, x; debug=[:Iteration, " ", :Cost, "\n", 1, :Stop])
